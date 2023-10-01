76f6837d226caa40a165591711612634c71f8b86
adarsh-2425 <adarsh.lol2425@gmail.com>
2023-10-01 17:24:51 +0530

    basic crud request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,24 @@
-import {Controller, Get} from "@nestjs/common"
+import {Controller, Delete, Get, Post} from "@nestjs/common"
 
-@Controller() // This decorator now gives the current entity to be a controller
+@Controller('report/:type') // Controller decorator now gives the current entity to be a controller
 export class AppController {
   @Get() // This decorator allows the below method to return as GET endpoint
   getAllIncomeReports() {
-    return []
+    return [];
+  }
+
+  @Get(':id') 
+  getAllIncomeReportsById() {
+    return {};
+  }
+
+  @Post()
+  postIncomeReports() {
+    return 'post ok. status 201'
+  }
+
+  @Delete(':id')
+  deleteIncomeReports() {
+    return 'delete ok. status 200'
   }
 }
