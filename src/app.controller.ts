dbfabe5f00ab4d01f751e4a6607e755c8af07207
adarsh-2425 <adarsh.lol2425@gmail.com>
2023-10-02 09:33:54 +0530

    validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post, Put, Body, HttpCode } from "@nestjs/common"
+import { Controller, Delete, Get, Param, Post, Put, Body, HttpCode, ParseUUIDPipe, ParseEnumPipe } from "@nestjs/common"
 import { ReportType, data } from 'src/data';
 import { v4 as uuid } from 'uuid';
 import { AppService } from "./app.service";
@@ -9,15 +9,15 @@ export class AppController {
   constructor(private readonly appService: AppService){}
 
   @Get() // This decorator allows the below method to return as GET endpoint
-  getAllReports(@Param('type') type: string) {
+  getAllReports(@Param('type', new ParseEnumPipe(ReportType)) type: string) {
     const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE; // checking the type in parameter. if "income", we assign type INCOME and vice versa
     return this.appService.getAllReports(reportType);
   }
 
   @Get(':id') 
   getAllReportsById(
-    @Param('type') type: string,
-    @Param('id') id: string
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string
   ) {
     const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE; // checking the type in parameter. if "income", we assign type INCOME and vice versa
     return this.appService.getReportById(reportType, id);
@@ -31,8 +31,8 @@ export class AppController {
 
   @Put(':id')
   updateReport(
-    @Param('type') type: string,
-    @Param('id') id: string,
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() body: { amount: number, source: string }
   ) {
     const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE; // checking the type in parameter. if "income", we assign type INCOME and vice versa
@@ -43,7 +43,7 @@ export class AppController {
   @HttpCode(204) // 204 means No Content, this controller will return 204
   @Delete(':id')
   deleteReport(
-    @Param('id') id: string
+    @Param('id', ParseUUIDPipe) id: string
   ) {
     this.appService.deleteREeport(id)
   }
